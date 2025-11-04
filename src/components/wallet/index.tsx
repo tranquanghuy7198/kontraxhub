850fe854ade18aeca1edd4da5b0a4afaf1a0b1da
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-11-04 15:59:12 +0700

    feat: Change wallet address style

--- src/components/wallet/index.tsx
+++ src/components/wallet/index.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { Wallet } from "@utils/wallets/wallet";
-import { Card, Image, Space, Tooltip } from "antd";
+import { Card, Image } from "antd";
 import { shorten } from "@utils/utils";
 import useNotification from "antd/es/notification/useNotification";
 import Paragraph from "antd/es/typography/Paragraph";
-import { useFetchBlockchains } from "@hooks/blockchain";
 import { useAppDispatch } from "@redux/hook";
 import { updateWallet } from "@redux/reducers/wallet";
 import "./wallet.scss";
@@ -16,7 +15,6 @@ const WalletCard: React.FC<{
   const [loading, setLoading] = useState<boolean>(false);
   const [notification, contextHolder] = useNotification();
   const dispatch = useAppDispatch();
-  const { blockchains } = useFetchBlockchains();
 
   const connectWallet = async (wallet: Wallet): Promise<void> => {
     try {
@@ -61,39 +59,14 @@ const WalletCard: React.FC<{
           <Image className="wallet-logo" preview={false} src={wallet.ui.icon} />
           <div className="wallet-title">
             <div className="wallet-name">{wallet.ui.name}</div>
-            <Space>
-              {wallet.address && wallet.chainId && (
-                <Tooltip
-                  title={
-                    blockchains.find(
-                      (blockchain) =>
-                        blockchain.chainId === wallet.chainId &&
-                        blockchain.networkCluster === wallet.networkCluster
-                    )?.name || `Unknown network (${wallet.chainId})`
-                  }
-                >
-                  <Image
-                    src={
-                      blockchains.find(
-                        (blockchain) =>
-                          blockchain.chainId === wallet.chainId &&
-                          blockchain.networkCluster === wallet.networkCluster
-                      )?.logo
-                    }
-                    preview={false}
-                    className="wallet-chain-logo"
-                  />
-                </Tooltip>
-              )}
+            {wallet.address && (
               <div
                 className="wallet-info"
-                style={{
-                  color: wallet.address ? "black" : "#f5222d",
-                }}
+                style={{ color: wallet.ui.titleColor }}
               >
-                {wallet.address ? shorten(wallet.address) : "Not Connected"}
+                {shorten(wallet.address)}
               </div>
-            </Space>
+            )}
           </div>
         </div>
       </Card>

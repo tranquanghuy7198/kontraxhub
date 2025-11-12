26c892e7ba7123abf41d567a356fa3f5efb4f510
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-11-12 13:40:11 +0700

    feat: Remove webhook

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,1 @@
 export const API_URL = "https://chainforge-backend-smef.onrender.com"; // TODO: env config
-export const WEBHOOK =
-  "https://discord.com/api/webhooks/1388933907277152256/qyMYIPSOjUxfkoLfIIqWlLiOXEStgIT2j1CM1VjUELjwbdki4FJMDlxzO-r4NkKc2h3t";

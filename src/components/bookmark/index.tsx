f8cac8888134c628e0e5a74b19a79df3e6c2db22
Quang-Huy Tran <tranquanghuy7198@gmail.com>
2025-11-20 16:39:05 +0700

    feat: Remove tooltip from bookmark

--- src/components/bookmark/index.tsx
+++ src/components/bookmark/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Tooltip } from "antd";
+import { Button } from "antd";
 import { StarFilled, StarOutlined } from "@ant-design/icons";
 import "./bookmark.scss";
 
@@ -19,19 +19,17 @@ const Bookmark: React.FC<BookmarkProps> = ({
   const value = isControlled ? checked : internalChecked;
 
   return (
-    <Tooltip title="Publish" placement="left">
-      <Button
-        type="text"
-        shape="circle"
-        size="small"
-        icon={value ? <StarFilled className="bookmark" /> : <StarOutlined />}
-        onClick={() => {
-          const newValue = !value;
-          if (!isControlled) setInternalChecked(newValue);
-          onChange?.({ target: { checked: newValue } });
-        }}
-      />
-    </Tooltip>
+    <Button
+      type="text"
+      shape="circle"
+      size="small"
+      icon={value ? <StarFilled className="bookmark" /> : <StarOutlined />}
+      onClick={() => {
+        const newValue = !value;
+        if (!isControlled) setInternalChecked(newValue);
+        onChange?.({ target: { checked: newValue } });
+      }}
+    />
   );
 };
 
